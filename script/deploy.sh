4eeccc9ae69588d4bc823781cee6d0f82c1c3d98
Jens Krause <jens@rasta.online>
2020-07-27 18:39:31 +0700

    encrypt password

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,5 +6,5 @@ JEKYLL_ENV="production" bundle exec jekyll build
 git status
 git add .
 git commit -m"[skip travis] Automated build"
-git remote set-url origin https://$SECRET@github.com/Solomonic/theming.rasta_online.git
+git remote set-url origin https://Solomonic:$PSW@github.com/Solomonic/my-new-website.git
 git push origin HEAD:master --force
